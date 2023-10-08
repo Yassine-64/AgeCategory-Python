f754abaa4540b1ec23a66c7f5c19842848eb1d14
Yassine mazhare <137923652+Yassine-64@users.noreply.github.com>
2023-10-08 13:56:00 +0200

    Add files via upload

---
+++ AgeCategoryClassifier.py
@@ -0,0 +1,12 @@
+
+age = int(input("Veuillez entrer l'âge de l'enfant : "))
+
+
+if age >= 6 and age <= 7:
+    print("Sa catégorie est poussin.")
+elif age >= 8 and age <= 9:
+    print("Sa catégorie est pupille.")
+elif age >= 10 and age <= 11:
+    print("Sa catégorie est minime.")
+elif age == 12:
+    print("Sa catégorie est cadet.")
